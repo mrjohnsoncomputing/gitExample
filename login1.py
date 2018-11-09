ca3f1e2b059e0b3d6b639b7afd68ae801b41dd92
Allan Callaghan <calaldees@gmail.com>
2018-11-09 10:29:38 +0000

    I added another user

--- login1.py
+++ login1.py
@@ -1,7 +1,7 @@
 print('begin')
 for attempt_number in range(3):
     username = input('Username: ')
-    if username in ('bob', 'jane', 'sally', 'allan','arnold', 'jim'):
+    if username in ('bob', 'jane', 'sally', 'allan','arnold', 'jim', 'shasha'):
         print(f'Welcome {username}')
         break
     print('Invalid login: go away!')
