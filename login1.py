f7f55da17b6722c6987d2579a562dc863a7e29ce
Allan Callaghan <calaldees@gmail.com>
2018-11-09 10:10:02 +0000

    I added error messages. Now the program is better Yay

--- login1.py
+++ login1.py
@@ -1,8 +1,10 @@
-
+print('begin')
 for attempt_number in range(3):
     username = input('Username: ')
     if username in ('bob', 'jane', 'sally'):
         print(f'Welcome {username}')
         break
+    print('Invalid login: go away!')
 else:
-    print(f'no')
+    print(f'You tried 3 times. Now I hate you')
+print('end')
