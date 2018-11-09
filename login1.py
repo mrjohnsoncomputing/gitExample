64812ffc35b469325a2971c34364dd21a1b269d6
mrjohnsoncomputing <42711962+mrjohnsoncomputing@users.noreply.github.com>
2018-11-09 10:48:19 +0000

    Security Patch
    Stopped Dr.Evil from being able to log in

--- login1.py
+++ login1.py
@@ -1,6 +1,9 @@
 print('begin')
 for attempt_number in range(3):
     username = input('Username: ')
+    if username = 'Dr Evil':
+        print('You are on perma-ban, please leave the internet.')
+        break
     if username in ('bob', 'jane', 'sally', 'allan','arnold', 'jim', 'shasha'):
         print(f'Welcome {username}')
         break
